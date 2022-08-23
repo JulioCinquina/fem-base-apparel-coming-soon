d3ab944c350d47c45a0a0c918602944b21b1d835
Julio Cinquina <66167767+JulioCinquina@users.noreply.github.com>
2022-08-23 13:16:10 -0300

    Add email validation script

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const errorMessageElement = document.querySelector('.form__error-message');
+const emailInputElement = document.querySelector('.form__input');
+const buttonElement = document.querySelector('.form__button');
+
+const showErrorMessage = function () {
+  errorMessageElement.textContent = 'Please provide a valid email';
+};
+
+const clearErrorMessage = function () {
+  errorMessageElement.textContent = '';
+};
+
+buttonElement.addEventListener('click', (event) => {
+  const emailIsValid = emailInputElement.checkValidity();
+
+  if (!emailIsValid) {
+    showErrorMessage();
+    event.preventDefault();
+  } else {
+    clearErrorMessage();
+  }
+});
