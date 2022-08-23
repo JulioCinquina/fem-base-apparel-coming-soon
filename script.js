2a59f81e131d3bc07885cbc2ce79d7da60200e75
Julio Cinquina <66167767+JulioCinquina@users.noreply.github.com>
2022-08-23 16:26:43 -0300

    Add error animation on input and button
    The input field and button will shake from side to side when the user
    clicks the button if invalid data has been entered.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const formElement = document.querySelector('.form');
 const errorMessageElement = document.querySelector('.form__error-message');
 const emailInputElement = document.querySelector('.form__input');
 const buttonElement = document.querySelector('.form__button');
@@ -12,12 +13,29 @@ const clearErrorMessage = function () {
   errorMessageElement.textContent = '';
 };
 
+const playErrorAnimation = function () {
+  // Adds animation classes
+  emailInputElement.classList.add('form__input--error-animation');
+  buttonElement.classList.add('form__button--error-animation');
+
+  // Removes classes when the animation ends
+  emailInputElement.addEventListener(
+    'animationend',
+    () => {
+      emailInputElement.classList.remove('form__input--error-animation');
+      buttonElement.classList.remove('form__button--error-animation');
+    },
+    { once: true }
+  );
+};
+
 buttonElement.addEventListener('click', (event) => {
   const emailIsValid = emailInputElement.checkValidity();
 
   if (!emailIsValid) {
     clearErrorMessage();
     showErrorMessage();
+    playErrorAnimation();
     event.preventDefault();
   } else {
     clearErrorMessage();
